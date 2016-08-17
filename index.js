16364c4d6fe5d7567c148c56ee88546b9149377a
Ihor Motornyi <igormotorny1997@gmail.com>
2016-08-17 19:05:01 +0300

    Change file name to hash

--- index.js
+++ index.js
@@ -1,73 +1,79 @@
-var fs = require('fs');
+const fs = require('fs');
+const md5 = require('js-md5');
 
-let isDirectory = (filename, path) => (fs.statSync(`${path}/${filename}`).isDirectory());
-let isFile = (filename, path) => fs.statSync(`${path}/${filename}`).isFile();
+let isDirectory = (path, filename) => (fs.statSync(`${path}/${filename}`).isDirectory());
+let isFile = (path, filename) => fs.statSync(`${path}/${filename}`).isFile();
 let isExists = (filename) => fs.existsSync(`${root}/${filename}`)
 let filterIgnoredFiles = (filename) => (gitignore.indexOf(filename) < 0);
 let mapStringToMD = (filename) => (
   `* [${filename}](https://github.com/IgorMotorny/library/blob/master/books/${filename}) `
 )
-let bookMapper = (path) => ({
 
-})
-let foderMapper = (filename, path) => ({[filename] : read(`${path}/${filename}`)})
-/**
-{
- general: [
- { filename: '...', path: '...' },
- ...
-],
-theme1: [
-{ filename: '...', path: '...' },
-...
-],
-[
-{ filename: '...', path: '...' },
-...
-]
-}
-*/
-/**
-{
-  general: [ 'Discover Meteor(Tom Coleman).pdf',  ... ],
-  Backbone: { general: [ 'Разработка Backbone.js приложений (2014,Эдди Османи)_.pdf' ] },
-  JQuery: { general:[ ... ] },
-  'Ng-book': { general: [ ... ] },
-  'You don\'t know js': { general:[ ... ] }
-}
+let folderMapper = (path, filename) => ({[filename] : read(`${path}/${filename}`)})
 
-*/
+let bookMapper = (path, filename) => ({
+  filename: filename,
+  path: `${path}/${filename}`,
+  hash: md5(filename)
+});
+
 function read(path) {
-   const dir = fs.readdirSync(path);
-   const files = dir.filter((filename) => isFile(filename, path))
-                    .filter(filterIgnoredFiles);
-   const directories = dir.filter((filename) => isDirectory(filename, path));
-   const a = directories.map((filename) => foderMapper(filename, path));
-   return Object.assign({}, {
-     general: files
-   }, ...a);
+  const dir = fs.readdirSync(path);
+
+  let isFileBinded = isFile.bind(null, path);
+  let bookMapperBinded = bookMapper.bind(null, path);
+  let isDirectoryBinded = isDirectory.bind(null, path);
+
+  const rootFiles = dir.filter(isFileBinded)
+    .filter(filterIgnoredFiles)
+    .map(bookMapperBinded);
+
+  const files = dir.filter(isDirectoryBinded)
+    .map((filename) => folderMapper(path, filename));
+
+  return Object.assign({}, { general: rootFiles }, ...files);
 }
 
-function createReadme(obj, path = []) {
-  let buff = [];
+function flatMap(obj) {
+  let bundle = [];
   if(obj instanceof Object && !Array.isArray(obj)) {
     for(key in obj) {
-      if(key !== 'general') {
-        const childPath = path.push(key);
-        buff.push(`\n## ${key}\n`);
-        buff.push(...createReadme(obj[key], childPath));
-      } else {
-        buff.push(...createReadme(obj[key]));
-      }
+      // console.log(flatMap(obj[key]));
+      bundle.push(...flatMap(obj[key]));
     }
   } else {
-    obj.forEach((item) => { buff.push(`\n* <a href="/books/${path.join('/') + item}) }">${item.replace(/\(.*?\)/, '')}</a> \n`); });
+    // Array
+    // copy file here
+    obj.forEach((item) => {
+      fs.writeFileSync(`./dist/${item.hash}.pdf`, fs.readFileSync(item.path));
+      bundle.push(`[${item.filename.replace(/.pdf/, '')}](./dist/${item.hash}.pdf)`)
+    });
   }
-  return buff;
+  return bundle;
 }
+//
+// function createReadme(obj) {
+//   let buff = [];
+//   if(obj instanceof Object && !Array.isArray(obj)) {
+//     for(key in obj) {
+//       if(key !== 'general') {
+//         const childPath = path.push(key);
+//         buff.push(`\n## ${key}\n`);
+//         buff.push(...createReadme(obj[key]));
+//       } else {
+//         buff.push(...createReadme(obj[key]));
+//       }
+//     }
+//   } else {
+//     obj.forEach((item) => { buff.push(`\n* <a href="./dist/${item.hash}.pdf) }">${item.replace(/\(.*?\)/, '')}</a> \n`); });
+//   }
+//   return buff;
+// }
 
 const root = './books';
 const gitignore = fs.readFileSync('.gitignore').toString().split('\n');
 const directory =  fs.readdirSync(root).filter(filterIgnoredFiles);
 
-fs.writeFile('README.md', `#Frontend books \n ${createReadme(read('./books')).join('')}`);
+fs.writeFile('README.md', `#Frontend books \n ${flatMap(read('./books')).join('\n')}`);
+// console.log();
+// flatMap(read('./books'))
